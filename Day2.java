b71cf59313a81f8855f6c868f9195c0684b3ac9f
vungochaianh <vungochaianh.ainow@gmail.com>
2023-12-07 23:18:14 +0700

    day 2

---
+++ Day2.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        System.out.println("Enter Number");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        switch (num){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("Three");
+                break;
+            case 4:
+                System.out.println("Four");
+                break;
+            case 5:
+                System.out.println("Five");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 10:
+                System.out.println("Ten");
+                break;
+        }
+    }
+}
